83970da5bcc462640aee6b7b650d6e566721a35d
CauaEmano <emcaua@gmail.com>
2026-05-16 20:08:16 -0300

    Elaborado classes rosa, banana e camera

---
+++ src/Camera.cpp
@@ -0,0 +1,15 @@
+#include "../include/Camera.h"
+#include "../include/Placar.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+Camera::Camera(int x, int y) : Coletavel(x, y, "📷"), ofuscada(false) {}
+
+/*void Camera::aplicarEfeito(Placar& placar){
+    Para futuro
+}*/
+
+std::string Camera::getTipo() const {return "Camera";}
+
+bool Camera::estaOfuscada() const {return ofuscada;}
---
+++ src/Rosa.cpp
@@ -0,0 +1,13 @@
+#include "../include/Rosa.h"
+#include "../include/Placar.h"
+#include <iostream>
+
+Rosa::Rosa(int x, int y, int pts) : Coletavel(x, y, "🌹"), pontos(pts) {}
+
+/*void Rosa::aplicarEfeito(Placar& placar) {
+    Futuro
+}*/
+
+std::string Rosa::getTipo() const { return "Rosa"; }
+
+int Rosa::getPontos() const {return pontos;}
