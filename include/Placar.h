12e02320a10496ea44c01aa650867f2c7188034f
ellesamarasllm <anesamaradelimaa@gmail.com>
2026-05-17 14:20:08 -0300

    Classe Placar

---
+++ include/Placar.h
@@ -0,0 +1,29 @@
+#ifndef PLACAR_H
+#define PLACAR_H
+#include <ostream>
+using namespace std;
+
+class Placar {
+private:
+	int rosas;
+	int bananas;
+	int cameras;
+public:
+	Placar();
+
+	void addRosas(int pts);
+	void zerarRosas();
+	int getRosas() const;
+
+	void incrementarBananas();
+	int getBananas() const;
+
+	void incrementarCameras();
+	int getCameras() const;
+
+	bool gameOver() const;
+
+	friend ostream& operator<<(ostream& os, const Placar& p);
+};
+
+#endif
