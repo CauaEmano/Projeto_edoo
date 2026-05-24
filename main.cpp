33fd028350b280bb05fa089cc5544a85a432d6cd
Jessica <jeeh.tmz@gmail.com>
2026-05-24 20:18:08 -0300

    feat: main feita e tratamento de erro adicionado em Jogo::processarEntrada(char entrada)

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "./include/Jogo.h"
+#include "./include/Fase.h"
+#include "./include/Placar.h"
+#include "./include/Gisele.h"
+#include "./include/Banana.h"
+#include "./include/Camera.h"
+#include "./include/Rosa.h"
+
+int main() {
+
+    Jogo novoJogo;
+    novoJogo.iniciar();
+    novoJogo.exibirResultado();
+
+    return 0;
+}
--- src/Jogo.cpp
+++ src/Jogo.cpp
@@ -1,6 +1,7 @@
 #include "../include/Jogo.h"
 #include <iostream>
 #include <vector>
+#include <stdexcept>
 
 using namespace std;
 
@@ -18,10 +19,21 @@ void Jogo::iniciar() {
         renderizar();
 
         char entrada;
-        cout << "Acao: ";
-        cin >> entrada;
+        bool entradaValida = false;
 
-        processarEntrada(entrada);
+        while (!entradaValida) {
+            cout << "Acao: ";
+            cin >> entrada;
+
+            try {
+                processarEntrada(entrada);
+            }
+            catch(const std::invalid_argument& e) {
+                std::cerr << "Erro: " << e.what();
+                continue;
+            }
+        }
+
         atualizar();
     }
 }
@@ -36,7 +48,7 @@ void Jogo::processarEntrada(char entrada) {
     } else if (entrada == 'q') {
         rodando = false;
     } else {
-        cout << "Entrada inválida!\n";
+        throw std::invalid_argument("Entrada inválida!\n");
     }
 }
 
